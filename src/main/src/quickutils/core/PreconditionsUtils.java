c49c0fdc2e1739d228dae35c2cdd605836f1e383
Ihor Karpachev <benchakalaka@gmail.com>
2014-11-25 12:57:07 +0000

    Added QPredictions method

--- src/main/src/quickutils/core/PreconditionsUtils.java
+++ src/main/src/quickutils/core/PreconditionsUtils.java
@@ -1,7 +1,7 @@
 package quickutils.core;
 
 import java.util.List;
-
+import java.util.Set;
 
 /**
  * Check preconditions as object is null, list is empty, etc ...
@@ -10,27 +10,39 @@ import java.util.List;
  */
 public class PreconditionsUtils {
 
-	PreconditionsUtils() {}
+     PreconditionsUtils () {
+     }
 
-	/**
-	 * Check list to null or empty value
-	 * 
-	 * @param <T>
-	 * @param jArray
-	 * @return
-	 */
-	public static boolean isNullOrEmpty(List<?> jArray) {
-		return (isNull(jArray)) || (jArray.isEmpty());
-	}
+     /**
+      * Check list to null or empty value
+      * 
+      * @param <T>
+      * @param jArray
+      * @return
+      */
+     public static boolean isNullOrEmpty(Set <?> jArray) {
+          return (isNull(jArray)) || (jArray.isEmpty());
+     }
 
-	/**
-	 * Check if object is null
-	 * 
-	 * @param obj
-	 *            object to check
-	 * @return true if object is null, otherwise return false
-	 */
-	public static boolean isNull(Object obj) {
-		return (null == obj);
-	}
+     /**
+      * Check list to null or empty value
+      * 
+      * @param <T>
+      * @param jArray
+      * @return
+      */
+     public static boolean isNullOrEmpty(List <?> jArray) {
+          return (isNull(jArray)) || (jArray.isEmpty());
+     }
+
+     /**
+      * Check if object is null
+      * 
+      * @param obj
+      *             object to check
+      * @return true if object is null, otherwise return false
+      */
+     public static boolean isNull(Object obj) {
+          return (null == obj);
+     }
 }
